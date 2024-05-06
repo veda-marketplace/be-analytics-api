5fd7633922b2e401c873a1b3efa729f3e17e12ea
Joan Flotats <96056718+joan-mido-qa@users.noreply.github.com>
2024-05-06 18:59:55 +0200

    lint: enable linting rules and enable Biome on CI (#3)
    Add BiomeJS checks to CI, enable the linting rules and fix some issues found by BiomeJS. Biome replaces Prettier + ESLint.

--- src/annotations/annotation.service.ts
+++ src/annotations/annotation.service.ts
@@ -1,41 +1,41 @@
-import axios from 'axios';
-import { Annotations } from './annotation.interfaces';
+import axios from "axios";
+import type { Annotations } from "./annotation.interfaces";
 
 function createRequest(imageContent: string) {
-    return {
-        requests: [
-            {
-                image: {
-                    content: imageContent
-                },
-                features: [
-                    {
-                        type: "LABEL_DETECTION",
-                        maxResults: 10
-                    },
-                    {
-                        type: "WEB_DETECTION",
-                        maxResults: 10
-                    }
-                ]
-            }
-        ]
-    }
-};
+	return {
+		requests: [
+			{
+				image: {
+					content: imageContent,
+				},
+				features: [
+					{
+						type: "LABEL_DETECTION",
+						maxResults: 10,
+					},
+					{
+						type: "WEB_DETECTION",
+						maxResults: 10,
+					},
+				],
+			},
+		],
+	};
+}
 
 const GCP_IMAGE_ANNOTATE_URL = "https://vision.googleapis.com/v1/images:annotate";
 
 export async function annotate(imageContent: string): Promise<Annotations> {
-    const request = createRequest(imageContent);
-    const response = await axios.post(GCP_IMAGE_ANNOTATE_URL, request, {
-        headers: {
-            'X-goog-api-key': process.env.GCP_VISION_API_KEY
-        }
-    });
-    if(response.status == 200) {
-        const annotations: Annotations =  response.data;
-        return annotations;
-    }
-    
-    throw new Error(response.statusText);
+	const request = createRequest(imageContent);
+	const response = await axios.post(GCP_IMAGE_ANNOTATE_URL, request, {
+		headers: {
+			"X-goog-api-key": process.env.GCP_VISION_API_KEY,
+		},
+	});
+	if (response.status === 200) {
+		const annotations: Annotations = response.data;
+		return annotations;
+	}
+
+	throw new Error(response.statusText);
 }
